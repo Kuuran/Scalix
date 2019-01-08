93c9b2b45f1998175be860be6617006c38c98f78
philcl <phil13420@live.fr>
2019-01-08 11:38:58 +0100

    Début

---
+++ scalix.c
@@ -0,0 +1,5 @@
+//
+// Created by PhilippeClaude on 08/01/2019.
+//
+
+#include "scalix.h"
---
+++ scalix.h
@@ -0,0 +1,8 @@
+//
+// Created by PhilippeClaude on 08/01/2019.
+//
+
+#ifndef PROJET_SCALIX_H
+#define PROJET_SCALIX_H
+
+#endif //PROJET_SCALIX_H
